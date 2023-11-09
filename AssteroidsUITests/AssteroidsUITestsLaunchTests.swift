1881dfd6091944a7fff787c6cfaadb4a34f6bd69
Marcus Kim <marcuskim1989@gmail.com>
2023-11-08 18:48:29 -0800

    Initial Commit

---
+++ AssteroidsUITests/AssteroidsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AssteroidsUITestsLaunchTests.swift
+//  AssteroidsUITests
+//
+//  Created by Marcus Y. Kim on 11/8/23.
+//
+
+import XCTest
+
+final class AssteroidsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
