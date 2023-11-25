4d64ca151d83f1656cd74fe49bed94b11f466ed7
Marcus Kim <marcuskim1989@gmail.com>
2023-11-25 11:38:08 -0800

    Add life gallery with a single spaceship to scene

--- Assteroids/GameScene.swift
+++ Assteroids/GameScene.swift
@@ -10,6 +10,7 @@ import GameplayKit
 
 class GameScene: SKScene {
     var scoreLabel: SKLabelNode!
+    var customContainer: CustomContainerNode!
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
     
@@ -21,6 +22,8 @@ class GameScene: SKScene {
      }
     
     override func sceneDidLoad() {
+        
+        customContainer = CustomContainerNode()
         
         if let view = self.view {
             self.size = view.bounds.size
@@ -35,13 +38,17 @@ class GameScene: SKScene {
         scoreLabel.horizontalAlignmentMode = .right
 
         addChild(scoreLabel)
+
+        customContainer.position.x = scoreLabel.position.x
+        customContainer.position.y = scoreLabel.position.y - 10
         
+        generateLife()
         
     }
     
     
     // Call this whenever the game starts, the user dies, and when we need to add ships to our lives gallery
-    func generateSpaceShip() -> SKSpriteNode? {
+    func generateSpaceShip(position: CGPoint = CGPoint(x: 0, y: 0)) -> SKSpriteNode? {
         if let paperPlaneSymbolImage = UIImage(systemName: "paperplane")?.withTintColor(.white) {
             
             let data = paperPlaneSymbolImage.pngData()
@@ -51,7 +58,7 @@ class GameScene: SKScene {
             
             spriteNode.anchorPoint = CGPoint(x: 0.5, y: 0.5)
             spriteNode.size = CGSize(width: CGFloat(50.0), height: CGFloat(50.0))
-            spriteNode.position = CGPoint(x: 0, y: 0)
+            spriteNode.position = position
             
            
             
@@ -72,6 +79,34 @@ class GameScene: SKScene {
         return nil
     }
     
+    func generateLife() {
+        if let paperPlaneSymbolImage = UIImage(systemName: "paperplane")?.withTintColor(.white) {
+            
+            let data = paperPlaneSymbolImage.pngData()
+            let newImage = UIImage(data: data!)
+            let texture = SKTexture(image: newImage!)
+            let spriteNode = SKSpriteNode(texture: texture)
+            
+            spriteNode.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            spriteNode.size = CGSize(width: CGFloat(50.0), height: CGFloat(50.0))
+                
+            customContainer.addNode(spriteNode)
+            //customContainer.addNode(spriteNode)
+                
+            addChild(customContainer)
+            //            let anchorPointInScene = spriteNode.convert(spriteNode.anchorPoint, to: self)
+            //
+            //            let isVisible = self.frame.contains(anchorPointInScene)
+            //
+            //            if isVisible {
+            //                print("Anchor point is within the visible area.")
+            //            } else {
+            //                print("Anchor point is outside the visible area.")
+            //            }
+            
+        }
+    }
+    
     func incrementScore() {
             // Call this method when the player earns points
             score += 10 // Adjust the score based on your game's rules
@@ -79,3 +114,29 @@ class GameScene: SKScene {
     
 }
 
+class CustomContainerNode: SKNode {
+    
+    var childNodes: [SKSpriteNode] = []
+
+    func addNode(_ node: SKSpriteNode) {
+        // Add the node to the container
+        
+        addChild(node)
+        
+        // Add the node to the array for reference
+        childNodes.append(node)
+        
+        // Arrange nodes based on their position in the array or any other logic
+        arrangeNodes()
+    }
+
+    private func arrangeNodes() {
+        // Your logic to arrange nodes, for example, line them up horizontally
+        var offsetX: CGFloat = 0
+
+        for node in childNodes {
+            node.position = CGPoint(x: offsetX, y: 0)
+            offsetX += node.size.width + 10  // Adjust the spacing between nodes
+        }
+    }
+}
