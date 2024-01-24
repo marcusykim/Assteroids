040249852237872fcc80cb432b90503d83173d29
Marcus Kim <marcuskim1989@gmail.com>
2024-01-23 20:27:06 -0800

    Add SmallerAsteroids class

--- Assteroids/GameScene.swift
+++ Assteroids/GameScene.swift
@@ -30,7 +30,7 @@ class GameScene: SKScene {
     var triggerNode: SKSpriteNode!
     var buttNode: [Int: SKSpriteNode] = [:]
     
-    let buttNodeMax: Int = 10
+    let buttNodeMax: Int = 1
     
     var score: Int = 0 {
          didSet {
@@ -547,29 +547,4 @@ class GameScene: SKScene {
     
 }
 
-class CustomContainerNode: SKNode {
-    
-    var childNodes: [SKSpriteNode] = []
 
-    func addNode(_ node: SKSpriteNode) {
-        // Add the node to the container
-        
-        addChild(node)
-        
-        // Add the node to the array for reference
-        childNodes.append(node)
-        
-        // Arrange nodes based on their position in the array or any other logic
-        arrangeNodes()
-    }
-
-    private func arrangeNodes() {
-        // Your logic to arrange nodes, for example, line them up horizontally
-        var offsetX: CGFloat = 0
-
-        for node in childNodes {
-            node.position = CGPoint(x: offsetX, y: 0)
-            offsetX += node.size.width - 37.5  // Adjust the spacing between nodes
-        }
-    }
-}
